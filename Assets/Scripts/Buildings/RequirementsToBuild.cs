40fe7156e53696760333877155e2dfd6865e8677
Chochosan1 <angel.pashev@abv.bg>
2020-09-18 15:19:30 +0300

    Resource prefabs
    Scriptable object assets for each resource
    Different villagers
    Buildings  - iron and gold mine

--- Assets/Scripts/Buildings/RequirementsToBuild.cs
+++ Assets/Scripts/Buildings/RequirementsToBuild.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Attached to every preview buildable object. These resources will be used to evaluate whether the building can be built or not.
+/// </summary>
 public class RequirementsToBuild : MonoBehaviour
 {
     public float woodRequired;
