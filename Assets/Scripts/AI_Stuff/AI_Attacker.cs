ffeba24f48f32a6e28e2d012e03427dfa74c8b73
Chochosan1 <angel.pashev@abv.bg>
2020-10-14 19:47:24 +0300

    -Tweaks to the Attacker AI + new additions
    -Attacker AI now supports Idle state
    -The attackers are now using other models + added animations to the states
    -An Ally prefab has been added and enemies and allies target each other in order to fight

--- Assets/Scripts/AI_Stuff/AI_Attacker.cs
+++ Assets/Scripts/AI_Stuff/AI_Attacker.cs
@@ -3,29 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public enum AIState { GoingToDefaultTarget, MovingToTarget, Attack}
+public enum AIState { Idle, GoingToDefaultTarget, MovingToTarget, Attack}
 
 public class AI_Attacker : AI_Base, IDamageable
 {
+    public bool debugState;
     [Header("Additional AI options")]
     private IDamageable currentDamageable;
     [SerializeField] private int attackerIndex;
     [SerializeField] private GameObject defaultTargetIfNoOtherAvailable;
     [SerializeField] private AI_Stats stats;
     [SerializeField] private float attackInterval = 1.5f;
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float runSpeed = 4.5f;
     [Tooltip("This stopping distance will be used by the agent that is attacking THIS target. This is useful when THIS target requires the agent to stop further from it (e.g big buildings)")]
     [SerializeField] private float customAgentStoppingDistance;
     [SerializeField] private float defaultAgentStoppingDistance = 1;
     private float attackAnimTimestamp;
     private AIState aiState;
     private NavMeshAgent agent;
+    private Animator anim;
     private float currentHealth;
     
-
-    // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     //    SetAgentTarget(agent, target.transform.position);
         aiState = AIState.GoingToDefaultTarget;
         defaultTargetIfNoOtherAvailable = GameObject.FindGameObjectWithTag("DefaultTargetToProtect");
@@ -39,7 +42,21 @@ public class AI_Attacker : AI_Base, IDamageable
     private void Update()
     {
         //ChooseNewTarget(true);
-        if (aiState == AIState.MovingToTarget)
+        if(aiState == AIState.Idle)
+        {
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isWalk", false);
+            anim.SetBool("isRun", false);
+            anim.SetBool("isAttack", false);
+            currentTarget = null;
+            currentDamageable = null;     
+            if(Vector3.Distance(defaultTargetIfNoOtherAvailable.transform.position, transform.position) > defaultAgentStoppingDistance)
+            {
+                GoToDefaultTarget();
+            }
+            ChooseNewTarget(true);
+        }
+        else if (aiState == AIState.MovingToTarget)
         {
             if (currentTarget == null || currentDamageable == null)
             {
@@ -48,13 +65,22 @@ public class AI_Attacker : AI_Base, IDamageable
                 return;
             }
             SetAgentDestination(agent, currentTarget.transform.position);
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalk", false);
+            anim.SetBool("isRun", true);
+            anim.SetBool("isAttack", false);
         }
         else if(aiState == AIState.Attack)
-        {       
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalk", false);
+            anim.SetBool("isRun", false);
+            anim.SetBool("isAttack", true);
             if (Time.time >= attackAnimTimestamp)
             {
                 if (currentTarget != null && currentDamageable != null)
                 {
+                    LookAtTarget();
                     currentDamageable.TakeDamage(stats.damage);
                     attackAnimTimestamp = Time.time + attackInterval;
                     Chochosan.ChochosanHelper.ChochosanDebug("Attack" + gameObject.name, "green");
@@ -70,11 +96,19 @@ public class AI_Attacker : AI_Base, IDamageable
         }
         else if(aiState == AIState.GoingToDefaultTarget)
         {
-            currentTarget = null;
-            currentDamageable = null;
+            //currentTarget = null;
+            //currentDamageable = null;
+            if (Vector3.Distance(defaultTargetIfNoOtherAvailable.transform.position, transform.position) <= defaultAgentStoppingDistance)
+            {
+                aiState = AIState.Idle;
+            }
             SetAgentDestination(agent, defaultTargetIfNoOtherAvailable.transform.position);
             agent.stoppingDistance = defaultAgentStoppingDistance;
             ChooseNewTarget(true);
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalk", true);
+            anim.SetBool("isRun", false);
+            anim.SetBool("isAttack", false);
         }
         
 
@@ -98,30 +132,55 @@ public class AI_Attacker : AI_Base, IDamageable
             }
             else
             {
-                GoIntoPatrolState();
+                GoToIdle();
+              //  GoToDefaultTarget();
             }
         }
         else
         {
-            GoIntoPatrolState();
+            GoToIdle();
+          //  GoToDefaultTarget();
+        }
+
+        if(debugState)
+        {
+            Chochosan.ChochosanHelper.ChochosanDebug(aiState.ToString(), "red");
         }
     }
 
-    private void GoIntoPatrolState()
+    private void GoToDefaultTarget()
     {
         if (aiState != AIState.GoingToDefaultTarget)
         {
+            agent.speed = walkSpeed;
             aiState = AIState.GoingToDefaultTarget;
             currentTarget = null;
             currentDamageable = null;
             SetAgentDestination(agent, agent.transform.position);
         }
     }
-    
+
+    private void GoToIdle()
+    {
+        if (aiState != AIState.Idle && aiState != AIState.GoingToDefaultTarget)
+        {
+            agent.speed = walkSpeed;
+            aiState = AIState.Idle;
+            currentTarget = null;
+            currentDamageable = null;
+            SetAgentDestination(agent, agent.transform.position);
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isWalk", false);
+            anim.SetBool("isRun", false);
+            anim.SetBool("isAttack", false);
+        }
+    }
+
     private void GoIntoMovingState()
     {
         if(aiState != AIState.MovingToTarget)
         {
+            agent.speed = runSpeed;
             aiState = AIState.MovingToTarget;
             currentDamageable = currentTarget.GetComponent<IDamageable>();
             if (currentDamageable != null)
@@ -149,7 +208,7 @@ public class AI_Attacker : AI_Base, IDamageable
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
-
+        Debug.Log("I TAKE DMG");
         if (currentHealth <= 0)
         {
             AI_Attacker_Loader.RemoveAttackerFromList(this);
